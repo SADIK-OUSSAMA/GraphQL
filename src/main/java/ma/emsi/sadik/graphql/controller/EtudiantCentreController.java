308e0c347af6bfe6b44ba3ce5fdd4706cfb44790
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-11-21 11:31:12 +0100

    added graphql schema

--- src/main/java/ma/emsi/sadik/graphql/controller/EtudiantCentreController.java
+++ src/main/java/ma/emsi/sadik/graphql/controller/EtudiantCentreController.java
@@ -11,6 +11,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Sinks;
 
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class EtudiantCentreController {
     EtudiantService etudiantService;
     @Autowired
     CentreService centreService;
+
     @QueryMapping
     public List<Centre> getAllCentres(){
         return centreService.centres();
